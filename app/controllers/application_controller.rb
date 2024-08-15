0a8178d7c334b92d7c17cfffe603df6af1ef5915
Inoue,T <inoue.t.796@ms.saitama-u.ac.jp>
2024-08-15 15:01:14 +0900

    Initialize repository

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,10 @@
+class ApplicationController < ActionController::Base
+
+    def hello
+        render html: "¡hello, mundo!"
+    end
+
+    def goodbye
+        render html: "goodbye, world!"
+    end
+end
